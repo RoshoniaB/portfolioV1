5032ff3a5e9dfe864051fcece86245e770668dbd
roshoniab <roshoniabrooks@gmail.com>
2020-11-30 10:30:33 -0600

    changed resume link and rearanged contact nav buttons

--- src/Components/ContactNav.js
+++ src/Components/ContactNav.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 export default function ContactNav() {
-    return (
-			<div>
-                <hi>hello</hi>
-				<a href='https://www.roshoniabrooks@gmail.com'>
-					<img className='email' src='img/email.png' alt='email' />
-				</a>
-				<a href='https://www.linkedin.com/in/roshonia-brooks/'>
-					<img className='linkedin' src='img/linkedin.png' alt='linkedin' />
-				</a>
-				<a href='https://github.com/RoshoniaB'>
-					<img className='github' src='img/github.png' alt='github' />
-				</a>
-			</div>
-		);
+	return (
+		<div>
+			<a href='https://www.roshoniabrooks@gmail.com'>
+				<img className='email' src='img/email.png' alt='email' />
+			</a>
+			<br />
+			<a href='https://www.linkedin.com/in/roshonia-brooks/'>
+				<img className='linkedin' src='img/linkedin.png' alt='linkedin' />
+			</a>
+			<br />
+			<a href='https://github.com/RoshoniaB'>
+				<img className='github' src='img/github.png' alt='github' />
+			</a>
+		</div>
+	);
 }
